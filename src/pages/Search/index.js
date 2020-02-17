2fecb7cfb71fa245d2278bd5c06b0a6bb857fb75
Amanda Tree <amandasimonds9@gmail.com>
2020-02-16 17:59:16 -0700

    changed files

---
+++ src/pages/Search/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Container from "../../components/Container";
+import SearchForm from "../../components/SearchForm";
+import SearchResults from "../../components/SearchResults";
+import Alert from "../../components/Alert";
+import EmployeeContext from "../../utils/EmployeeContext";
+import API from "../../utils/API";
+
+function Search() {
+  const [employeeState, setEmployeeState] = useState({
+    employees: [],
+    searchTerm: ""
+  });
+
+  const [search, setSearch] = useState("");
+  // const [error, setError] = useState("");
+
+  // When the component mounts, update the title to be Wikipedia Searcher
+  useEffect(() => {
+
+    API.searchTerms(search)
+      .then(res => {
+        if (res.data.length === 0) {
+          throw new Error("No results found.");
+        }
+        if (res.data.status === "error") {
+          throw new Error(res.data.message);
+        }
+        setEmployeeState({
+          employees: res.data.results,
+          searchTerm: ""
+        });
+      })
+      .catch(err => setError(err));
+  }, [search]);
+
+  const handleInputChange = event => {
+    setSearch(event.target.value);
+  };
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+  };
+  return (
+    <EmployeeContext.Provider value={employeeState}>
+      <div>
+        <Container style={{ minHeight: "100vh" }}>
+          <h1 className="text-center">Search For Anything on Wikipedia</h1>
+          <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
+            {error}
+          </Alert>
+          <SearchForm
+            handleFormSubmit={handleFormSubmit}
+            handleInputChange={handleInputChange}
+            results={search}
+          />
+          <SearchResults />
+        </Container>
+      </div>
+    </EmployeeContext.Provider>
+  );
+}
+
+export default Search;
